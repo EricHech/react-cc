50401f1bba7c72085ded43fb5039a0cf6cb23d11
Eric <erichech@gmail.com>
2018-05-23 20:38:33 -0400

    finished secondp problem

--- src/UserInput.js
+++ src/UserInput.js
@@ -3,7 +3,22 @@ import React, { Component } from 'react';
 class UserInput extends Component {
   state = {
     input: '',
+    fetched: {},
+    reveal: false,
   };
+
+  componentDidMount() {
+    fetch(
+      'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke'
+    )
+      .then((res) => res.json())
+      .then((fetched) => {
+        this.setState({ fetched });
+      })
+      .catch((err) => {
+        console.err(err);
+      });
+  }
 
   onInputChange = (event) => {
     event.preventDefault();
@@ -11,8 +26,12 @@ class UserInput extends Component {
   };
 
   clearForm = () => {
-    this.setState ({ input: ''})
-  }
+    this.setState({ input: '' });
+  };
+
+  displayJoke = () => {
+    return <div>Punchline: {this.state.fetched.punchline}</div>;
+  };
 
   render() {
     return (
@@ -25,7 +44,18 @@ class UserInput extends Component {
           value={this.state.input}
           onChange={this.onInputChange}
         />
-        <button type='button' onClick={this.clearForm}>Clear</button>
+        <button type="button" onClick={this.clearForm}>
+          Clear
+        </button>
+        <h1>Some Jokes For You:</h1>
+        <div>Setup: {this.state.fetched.setup}</div>
+        <button
+          type="button"
+          onClick={() => this.setState({ reveal: !this.state.reveal })}
+        >
+          Toggle Punchline
+        </button>
+        {this.state.reveal ? this.displayJoke() : <div />}
       </div>
     );
   }
