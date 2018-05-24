1d63a85eb6c0aab3d834042f39fbb25d66090b94
Eric <erichech@gmail.com>
2018-05-23 20:09:58 -0400

    finished

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 
 class UserInput extends Component {
-  render() { 
-    return ( 
+  state = {
+    input: '',
+  };
+
+  onInputChange = (event) => {
+    event.preventDefault();
+    this.setState({ input: event.target.value });
+  };
+
+  clearForm = () => {
+    this.setState ({ input: ''})
+  }
+
+  render() {
+    return (
       <div>
-        <h1> Your code goes here</h1>
+        <h1>This dat types:</h1>
+        <h1>Output: {this.state.input}</h1>
+        <input
+          type="text"
+          placeholder="Input"
+          value={this.state.input}
+          onChange={this.onInputChange}
+        />
+        <button type='button' onClick={this.clearForm}>Clear</button>
       </div>
-    )
+    );
   }
 }
 
